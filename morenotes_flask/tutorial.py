26c79d81de19d18bdad2cb112d7ce1ccb6817b65
eliasperkins-jpg <elias.perkins@ucas-edu.net>
2025-12-03 12:51:45 -0700

    lemin

--- morenotes_flask/tutorial.py
+++ morenotes_flask/tutorial.py
@@ -2,22 +2,21 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/" methods=["POST, GET"])
+@app.route("/", methods=["POST", "GET"])
 def home():
     if request.method == "POST":
-        return redirect(url_for("user", name=request. form["name"]))
-    return "<h1>Hello!</h1>"
+        return redirect(url_for("user", name=request.form["name"]))
+    return render_template("index.html")
 
 @app.route("/<name>")
 def user(name):
-
-    
+    return f"<h1>Hello {name}</h1>"
 
 @app.route("/contact")
 def contact():
     return "<p>LEAVE ME ALONE</p>"
 
-    return f"<h1>Hello {name}</h1>"
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
